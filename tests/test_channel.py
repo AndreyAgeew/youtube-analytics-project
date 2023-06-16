5f703e67b1ab243ed66928792a7158671b2f951a
Andrey Ageew <faceofdeviler@gmail.com>
2023-06-16 04:47:45 +0300

    add tests for magics methods

--- tests/test_channel.py
+++ tests/test_channel.py
@@ -30,3 +30,33 @@ def test_to_json(channel, tmpdir):
     assert channel_data['subscriberCount'] == channel._Channel__subscriber_count
     assert channel_data['videoCount'] == channel._Channel__video_count
     assert channel_data['viewCount'] == channel._Channel__view_count
+
+
+def test_str(channel):
+    expected_str = "MoscowPython (https://www.youtube.com/channel/UC-OVMPlMA3-YCIeg4z5z23A)"
+    assert str(channel) == expected_str
+
+
+def test_eq(channel, channel_second):
+    channel._Channel__subscriber_count, channel_second._Channel__subscriber_count = 1000, 1000
+    assert channel == channel
+
+
+def test_lt(channel, channel_second):
+    channel._Channel__subscriber_count, channel_second._Channel__subscriber_count = 500, 1000
+    assert channel < channel_second
+
+
+def test_add(channel, channel_second):
+    channel._Channel__subscriber_count, channel_second._Channel__subscriber_count = 500, 1000
+    assert (channel + channel_second) == 1500
+
+
+def test_rsub(channel, channel_second):
+    channel._Channel__subscriber_count, channel_second._Channel__subscriber_count = 1000, 500
+    assert (channel_second - channel) == -500
+
+
+def test_sub(channel, channel_second):
+    channel._Channel__subscriber_count, channel_second._Channel__subscriber_count = 1000, 500
+    assert (channel - channel_second) == 500
