4b465b1f5639277e55fc469e9331ee056fbb8572
Andrey Ageew <faceofdeviler@gmail.com>
2023-06-16 03:06:12 +0300

    change channel.py: class - change func, add attributes

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,7 @@
+import json
+from googleapiclient.discovery import build
+
+from src.config import API_KEY_YOUTUBE
 
 
 class Channel:
@@ -5,8 +9,18 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.__channel_id = channel_id
+        self.__youtube = build('youtube', 'v3', developerKey=API_KEY_YOUTUBE).channels(). \
+            list(id=self.__channel_id, part='snippet,statistics').execute()
+
+    @property
+    def channel_id(self):
+        return self.__channel_id
+
+    @channel_id.setter
+    def channel_id(self, value):
+        self.__channel_id = value
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        print(json.dumps(self.__youtube, indent=2, ensure_ascii=False))
